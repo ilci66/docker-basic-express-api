9933d058a4d15b4ecf602474ce62a39c1bef8438
ilci66 <akbiyik.ilker7@gmail.com>
2022-01-03 08:52:03 +0100

    caching package.json

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const port = 3000
 
 app.get('/', (req, res) => {
   res.json([
-    { name: 'ilker', email: 'ilker@hotmail.com' }
+    { name: 'ilker', email: 'ilker@hotmail.com' },
+    { name: 'user1', email: 'user1@hotmail.com' },
+    { name: 'user2', email: 'user2@hotmail.com' },
+    { name: 'user3', email: 'user3@hotmail.com' }
   ])
 });
 
